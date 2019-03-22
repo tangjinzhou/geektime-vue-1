73284a2f059146d3f0e43c3eda4aa1c3504c1961
tangjinzhou <415800467@qq.com>
2019-03-22 20:04:32 +0800

    add my-app

---
+++ my-app/src/components/TodoItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <li class="item">
+    <input type="checkbox" v-model="checked">
+    <slot name="item" v-bind="{checked}"></slot>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['item'],
+  data() {
+    return {
+      checked: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.item {
+    color: red;
+}
+</style>
