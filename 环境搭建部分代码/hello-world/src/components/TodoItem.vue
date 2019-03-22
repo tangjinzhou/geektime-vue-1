7d5e8b2092d677c3fe33f58659a9eb8247936259
tangjinzhou <415800467@qq.com>
2019-03-22 20:34:14 +0800

    add 环境

---
+++ 环境搭建部分代码/hello-world/src/components/TodoItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <li class="text">{{ todo.text }}</li>
+</template>
+
+<script>
+export default {
+  props: ['todo'],
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.text {
+  color: #42b983;
+}
+</style>
