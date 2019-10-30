799b4f86fa4c23a5171136e40ecf365823424235
tangjinzhou <415800467@qq.com>
2019-10-30 20:58:28 +0800

    update demo

--- 演示DEMO源码/src/router.js
+++ 演示DEMO源码/src/router.js
@@ -12,21 +12,6 @@ export default new Router({
       component: Home
     },
     {
-      path: "/1.1",
-      name: "Vue三大核心概念(属性、事件、插槽)",
-      component: () => import("./views/1.1")
-    },
-    {
-      path: "/1.2",
-      name: "双向绑定和单向数据流不冲突",
-      component: () => import("./views/1.2")
-    },
-    {
-      path: "/1.3",
-      name: "虚拟Dom",
-      component: () => import("./views/1.3")
-    },
-    {
       path: "/1.4",
       name: "如何触发组件的更新",
       component: () => import("./views/1.4")
@@ -60,11 +45,6 @@ export default new Router({
       path: "/1.10",
       name: "template 和 JSX 对比及其他们的本质",
       component: () => import("./views/1.10")
-    },
-    {
-      path: "/1.11",
-      name: "习题解答",
-      component: () => import("./views/1.11")
     }
   ]
 });
